ac63a9cccd7b53246e1a19be24bfc5e6170c874f
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-03-13 00:12:41 -0600

    chapter_2

---
+++ apostrophe.py
@@ -0,0 +1,10 @@
+message = "One of Python's greatest strengths is its diverse community"
+print(message)
+
+name = 'Eric'
+greeting = f'Hello, {name}. Would you like to learn Python today?'
+print(greeting)
+
+quote =  "My mind becomes a familiar terror, and I become a familiar terrorist"
+print("\t"+ quote)
+
