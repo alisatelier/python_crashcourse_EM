4616e2466db601abdfe84b1bf55dbba549442e15
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-03-15 02:39:39 -0600

    chapter_3

---
+++ exercise3_1.py
@@ -0,0 +1,12 @@
+names = ['sarah', 'christine', 'nicole', 'mike']
+message = f"Hello {names[0].title()}!"
+print(message)
+
+message2 = f"{names[1].title()} has really nice eyes."
+print(message2)
+
+message3 = f"I think {names[-2].upper()} would appreciate coffee sometime!"
+print(message3)
+
+message4 = f"Matt and/or {names[-1].title()} is a 'special guy', so says {names[1].title()}. \nI agree."
+print(message4)
