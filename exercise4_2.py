0c5bce697e49550b44d9edff169028511c62915f
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-03-19 00:25:17 -0600

    chapter_4

---
+++ exercise4_2.py
@@ -0,0 +1,5 @@
+our_cats = ['ella', 'chanman', 'luna', 'sage', 'charles']
+for a_cat in our_cats:
+    print(f"{a_cat.title()} is a perfect angel. You could not find a better cat.\n")
+
+print("One of these angels has gone to the dark side. I'll let you guess which one.")
---
+++ magicians.py
@@ -0,0 +1,9 @@
+magicians = ['alice','david', 'carolina']
+for magician in magicians:
+
+
+    print(f"{magician.title()}, that was a great trick!")
+
+    print(f"I can't wait to see your next trick, {magician.title()}\n")
+
+print(magician)# only prints the final vlaue, as it is not indented.
