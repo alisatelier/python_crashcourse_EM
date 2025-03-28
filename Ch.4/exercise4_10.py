215be82a6d75a84007e3c7f0b3e69eb8963ab6a9
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-03-28 13:24:35 -0600

    chapter4_3

---
+++ Ch.4/exercise4_10.py
@@ -0,0 +1,13 @@
+our_cats = ['ella', 'chanman', 'luna', 'sage', 'charles']
+for a_cat in our_cats:
+    print(f"{a_cat.title()} is a perfect angel. You could not find a better cat.\n")
+
+print("One of these angels has gone to the dark side. I'll let you guess which one.")
+
+print("The best two cats in this list are:")
+print(our_cats[0:2])
+
+print(f"The next best cats are: {our_cats[2:4]}")
+
+print(f"The cat that is on probation is: {our_cats[-1]}")
+
---
+++ Ch.4/foods.py
@@ -0,0 +1,12 @@
+my_foods = ['sushi', 'tacos', 'dessert crepes']
+friend_foods = my_foods[:]
+
+my_foods.append('pho')
+friend_foods.append('ramen')
+
+print("My favourite foods are:")
+print(my_foods)
+
+print("\nMy Friend's favourite foods are:")
+print(friend_foods)
+
